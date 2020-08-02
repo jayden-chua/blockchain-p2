3f0fb26e13758f9f7688c93071599eb1925a199b
Jayden Chua <gjiehs@gmail.com>
2020-08-02 16:23:16 +0800

    fix validate function

--- src/block.js
+++ src/block.js
@@ -40,8 +40,10 @@ class Block {
         return new Promise((resolve, reject) => {
             // Save in auxiliary variable the current block hash
             let currentHash = self.hash;
+            self.hash = null;
             // Recalculate the hash of the Block
             let calculatedHash = SHA256(JSON.stringify(self)).toString();
+            self.hash = currentHash;
             // Comparing if the hashes changed
             if (currentHash !== calculatedHash) {
                 // Returning the Block is not valid
